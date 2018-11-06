f4547d0529962ce36041f4f5748095146f95a48f
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-11-06 11:54:05 +0100

    update

--- Documentacion/Html/LISTA_DE_PRODUCTO/listaproducto.js
+++ Documentacion/Html/LISTA_DE_PRODUCTO/listaproducto.js
@@ -1,3 +1,52 @@
+//Se llama al body
+document.body.onload= function(){
+    //Conexion con ajax json
+    $.ajax({
+        url:"http://www.mocky.io/v2/5be16e253000004b00d9a982"
+    }).done(function (listaProducto)
+    {
+        addProducto("#contenedorLista",listaProducto)
+    }).fail(function(error){
+        console.log("Error:",error)
+    });
+    
+}
+//Se añade el producto
+function addProducto(id,listaProducto)
+{
+    $(id).html(function()
+        {
+
+            let htmlProductos=""
+            
+          listaProducto.forEach(productos => {
+              //carga el html
+             htmlProductos +=`
+            <li class="list-group-item">
+                
+                <div class="container">
+
+                    <div class="col-sm-5"><img class="img-thumbnail" src="${productos.imagen}" width="200" height="120" alt=""></div>
+                        
+                    <div class="col-sm-5">
+                        <div><h3><a href="./ProductoDetallado.html" class="" >${productos.producto}</a></h3></div>
+                            
+                        <p class="listado">${productos.especificaciones}</p>
+                        
+                        <h4>${productos.precio}</h4>
+                    </div>
+                               
+                </div>
+                
+            </li>
+            `   
+              
+          });
+           
+
+        $(id).append(htmlProductos);
+        });
+}
 var corazon = document.getElementById("btncorazon");
 
 corazon.onclick = function () {
